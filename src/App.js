be87e52f0ba7f16f9f2e55107300af876cfc5eb5
Eduardo Alarcon <eduardoalarcon111@gmail.com>
2024-08-02 15:04:38 -0300

    aula 07

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Banner from "./Components/Banner";
 import Container from "./Components/Container";
-
+import Cards from "./Components/Cards";
+import Category, {categories, filterCategory} from "./Components/Category";
 
 function App() {
   return (
     <>
-    <Header />
-    <Banner image = "home" />
-    <Container>
-   <h1>Hello World!</h1>
-    </Container>
-   <Footer />
+      <Header />
+      <Banner image="home" />
+      <Container> 
 
-  </>
+      {
+        categories.map((category, index) => 
+          <Category category={category}>
+     { filterCategory(index).map((video) => <Cards id={video.id} key={video.id} /> )}
+      </Category>
+        )
+      }
+
+
+      </Container>
+      <Footer />
+
+    </>
   );
 }
 
